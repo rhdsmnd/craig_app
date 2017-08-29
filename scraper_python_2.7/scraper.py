6630c9543895c2401369e0bf9d9bb6a9a2df2bdf
Ron Desmond <rhdsmnd@Rons-MacBook-Pro.local>
2017-08-28 18:10:31 -0700

    Scraper updates.

--- scraper_python_2.7/scraper.py
+++ scraper_python_2.7/scraper.py
@@ -45,7 +45,9 @@ def getProjectRootDir():
     return fileName[0: fileName.rfind(os.sep) + 1]
 
 
-def setupDb(db):
+def setupDb(dbName):
+    db = sqlite3.connect(dbName)
+
     db.executescript("""
         CREATE TABLE IF NOT EXISTS Listing(
             id INTEGER PRIMARY KEY,
@@ -91,6 +93,7 @@ def setupDb(db):
             defaultLastSeen["min"]
         ))
     db.commit()
+    return db
 
 
 def parseDt(craigDt):
@@ -305,13 +308,32 @@ def updateLastSeenDb(newDt, db):
     db.commit()
     return
 
-def start():
+def computePrefs(input):
+
+    # get desired locations from database
+    # put each location into prefs
+
+
+    res = {
+        "qParams": {
+            "br" : [3, 3],
+            "max_price" : 5450,
+            "min_price" : 3000,
+            "sort" : "date"
+        }
+    }
+
+    # for key in input, replace key in res if it is an official key
+
+    return res
+
+def start(inputPrefs):
     print "Running scraper."
 
-    db = sqlite3.connect('craig_posts.sqlite3')
-    setupDb(db)
+    scraperPrefs = computePrefs(inputPrefs)
+    db = setupDb(scraperPrefs["db_name"])
     lastSeenDb = getLastSeenDb(db)
-    maxToSee = 1080
+    maxToSee = total = 1080
 
     try:
         s = 0
@@ -319,9 +341,10 @@ def start():
         seenDuplicate = False
         updateSeenDt = None
         saved = []
-        while (s < maxToSee and not seenDuplicate):
+        while (s < maxToSee and s < total and not seenDuplicate):
 
-            qParams = {"sort": "date", "max_price": 1956, "min_price" : 1200, "s": s}
+            qParams = scraperPrefs["qParams"]
+            qParams["s"] = s
             parsed = makeReq("https://sfbay.craigslist.org/search/apa", qParams)
 
             selRows = CSSSelector('.result-row')
@@ -335,7 +358,7 @@ def start():
                 print "No listings for query with parameters: " + str(qParams)
                 break
             else:
-                total = selTotal(parsed)[0].text
+                total = int(selTotal(parsed)[0].text)
                 start = selStart(parsed)[0].text
                 end = selEnd(parsed)[0].text
                 print "Retrieved " + str(start) + " to " + str(end) + " of " + str(total) + " listings."
